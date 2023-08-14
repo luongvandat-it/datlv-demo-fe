f5f384f86debbc9ebbd1480629c8b4b4cb4310e0
LuongVanDat <luongvandat.it@gmail.com>
2023-08-14 10:09:44 +0700

    #6 - fix unlink sometime not work

--- js/main.js
+++ js/main.js
@@ -1,68 +1,46 @@
-(function ($) {
-    "use strict";
-    $('.input100').each(function () {
-        $(this).on('blur', function () {
-            if ($(this).val().trim() != "") {
-                $(this).addClass('has-val');
+$(document).ready(() => {
+    localStorage.clear();
+    $('#btnLoginManual').click(() => {
+        const userLogin = {
+            // email: $('#txtLoginEmail').val(),
+            // password: $('#txtLoginPassword').val()
+            email: 'datlv@gmail.com',
+            password: 'Datlv@12345'
+        };
+        $.ajax({
+            url: 'http://localhost:3000/graphql',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({
+                query: `
+                        query {
+                            login(email: "${userLogin.email}", password: "${userLogin.password}") {
+                                accessToken
+                                ownerId
+                            }
+                        }
+                    `
+            }),
+            success: (res) => {
+                console.log(res);
+                if (res.data.login) {
+                    const dataLogin = {
+                        email: userLogin.email,
+                        accessToken: res.data.login.accessToken,
+                        ownerId: res.data.login.ownerId
+                    };
+                    localStorage.setItem('dataLogin', JSON.stringify(dataLogin));
+
+                   
+
+                    window.location.href = './html/home.html';
+                } else {
+                    alert('Login failed!');
+                }
+            },
+            error: (err) => {
+                console.log(err);
             }
-            else {
-                $(this).removeClass('has-val');
-            }
-        })
-    })
-
-    var input = $('.validate-input .input100');
-    $('.validate-form').on('submit', function () {
-        var check = true;
-
-        for (var i = 0; i < input.length; i++) {
-            if (validate(input[i]) == false) {
-                showValidate(input[i]);
-                check = false;
-            }
-        }
-
-        if (check == true) {
-            window.location.href = "../html/home.html";
-            return false;
-
-        }
-        return check
-    });
-
-
-    $('.validate-form .input100').each(function () {
-        $(this).focus(function () {
-            hideValidate(this);
         });
     });
-
-    function validate(input) {
-        if ($(input).val().trim() == '') {
-            return false;
-        }
-        else if ($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
-            var re = /\S+@\S+\.\S+/;
-            if (re.test($(input).val().trim()) == false) {
-                return false;
-            }
-        }
-        else if ($(input).attr('type') == 'password' || $(input).attr('name') == 'password') {
-            var re = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/;
-            if (re.test($(input).val().trim()) == false) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    function showValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).addClass('alert-validate');
-    }
-
-    function hideValidate(input) {
-        var thisAlert = $(input).parent();
-        $(thisAlert).removeClass('alert-validate');
-    }
-})(jQuery);
+});
